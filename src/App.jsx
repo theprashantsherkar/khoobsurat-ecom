8298a0f1b59cc5b62677d428e6cd8a060c4e5808
Prashant <prashantsherkar7515400@gmail.com>
2025-08-13 14:31:37 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='flex items-center justify-center h-screen bg-gray-500 w-screen'>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default App
